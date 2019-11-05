26556bdd56ab29b0c740f5eb252705b777d7815d
Chrupcio <chrupcion2o@gmail.com>
2019-11-05 18:56:59 +0100

    Add relations to models

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,4 @@
 class Reservation < ApplicationRecord
+  has_many :payments
   belongs_to :ticket
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,4 @@
 class Ticket < ApplicationRecord
+  has_many :reservations
   belongs_to :event
 end
